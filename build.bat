62368d213c3acd7d731c8823cceea918d7659a2c
unknown <jonathums@gmail.com>
2024-05-06 22:21:30 -0500

    build: windows build script uses same ldflags as bash script

--- build.bat
+++ build.bat
@@ -1,19 +1,21 @@
+SET LDFLAGS="-X 'main.buildDate=$(date)' -X 'main.gitHash=$(git rev-parse HEAD)' -X 'main.buildOn=$(go version)' -w -s"
+
 SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
-go build -o gml-linux -trimpath -ldflags "-w -s"  
+go build -o gml-linux -trimpath -ldflags %LDFLAGS%
 
 SET CGO_ENABLED=0
 SET GOOS=darwin
 SET GOARCH=amd64
-go build -o gml-darwin -trimpath -ldflags "-w -s"  
+go build -o gml-darwin -trimpath -ldflags %LDFLAGS%
 
 SET CGO_ENABLED=0
 SET GOOS=darwin
 SET GOARCH=arm64
-go build -o gml-arm-darwin -trimpath -ldflags "-w -s"  
+go build -o gml-arm-darwin -trimpath -ldflags %LDFLAGS%
 
 SET CGO_ENABLED=0
 SET GOOS=windows
 SET GOARCH=amd64
-go build -o gml-windows.exe -trimpath -ldflags "-w -s"  
+go build -o gml-windows.exe -trimpath -ldflags %LDFLAGS%
